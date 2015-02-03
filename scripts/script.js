308fdd0cc67fac9b7a40386cbd98cb6f86421777
Matt Liebetrau <matt.liebetrau@gmail.com>
2015-02-02 20:51:03 -0500

    finished first rough draft

--- scripts/script.js
+++ scripts/script.js
@@ -6,4 +6,21 @@ $(document).ready(function(){
      $(".third").toggleClass("open");
   });
 
+  	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+	$('html, body').stop().animate({
+	    'scrollTop': $target.offset().top
+	}, 900, 'swing');
+
 });
